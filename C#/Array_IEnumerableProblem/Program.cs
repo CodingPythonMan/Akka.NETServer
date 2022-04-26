b6984c15501c62d7c46f0f35fb085ae245211ac8
robo <robolab1902@naver.com>
2022-04-26 23:16:28 +0900

    C# 공부 1일차

---
+++ C#/Array_IEnumerableProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Array_IEnumerableProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] employees = { "Joe", "Bob", "Carol", "Alice", "Will" };
+
+            //제네릭이 아닌 유형 또는 메소드 '식별자'는 유형 인수와 함께 사용할 수 없습니다.
+            IEnumerable<string> employeeQuery =
+                from person in employees
+                orderby person
+                select person;
+
+            foreach(string employee in employeeQuery)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
