0a8ae09abca1c12c54fbdfe7194ccde21aca39e1
JuYoung Kim <robolab1902@naver.com>
2023-11-27 19:36:14 +0900

    C# 으로 변경

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using IDLCompiler.Service;
+
+const string PACKET_FILE = "PacketInfo.dat";
+MakeSourceService service = new();
+
+// 1. 파일 읽기
+service.ReadFile(PACKET_FILE);
+
+// 2. 소스파일 생성하기
+service.MakeProxyFile();
+service.MakeStubFile();
---
+++ Service/MakeSourceService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDLCompiler.Service
+{
+    public class MakeSourceService
+    {
+        const string PROXY_CPP_FILE = "Proxy.cpp";
+        const string PROXY_H_FILE = "Proxy.h";
+
+        const string SERIALIZE = "Packet";
+
+        public void ReadFile(string ConfigFile)
+        {
+            StreamReader reader = File.OpenText(ConfigFile);
+            string line;
+            while((line = reader.ReadLine()!) != null)
+            {
+
+
+
+            }
+        }
+
+        public void MakeProxyFile()
+        {
+            
+        }
+
+        public void MakeStubFile()
+        {
+
+        }
+    }
+}
