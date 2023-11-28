d58a40560bd128892b6957fb04928a288b16d200
JuYoung Kim <robolab1902@naver.com>
2023-11-28 18:23:18 +0900

    Proxy CPP, H에 const reference 형태로 올라갈 수 있도록 작업해야한다

--- Service/MakeSourceService.cs
+++ Service/MakeSourceService.cs
@@ -8,6 +8,13 @@ namespace IDLCompiler.Service
 {
     public class MakeSourceService
     {
+        const int NORMAL_ARGUMENT = 2;
+        const int EXCEPT_ARGUMENT = 1;
+
+        const string TYPE = "Type";
+        const string VARIABLE = "Variable";
+
+
         const string PROXY_CPP_FILE = "Proxy.cpp";
         const string PROXY_H_FILE = "Proxy.h";
 
@@ -32,29 +39,74 @@ namespace IDLCompiler.Service
                 {
                     if (char.IsLetter(lines[i][j]))
                     {
-                        _ValidLines.Add(lines[i]);
-
+                        // 이 곳에서 반드시 넣어줘야하는 변수를 추가해 넣는다. 
+                        int SemiIndex = lines[i].IndexOf(';');
+                        string Valid = lines[i].Substring(j, SemiIndex);
+                        _ValidLines.Add(sb.ToString());
                         break;
                     }
                 }
             }
         }
 
-        public Dictionary<string, List<string>> AnalyzeLine(string Line)
+        private Dictionary<string, List<string>> AnalyzeLine(string Line)
         {
             Dictionary<string, List<string>> result = new();
 
             int Start = Line.IndexOf('(') + 1;
             int End = Line.IndexOf(')');
 
-            string str = Line.Substring(Start, End - Start);
+            string ArgumentStr = Line.Substring(Start, End - Start);
 
+            if (true == string.IsNullOrEmpty(ArgumentStr))
+                return result;
 
+            var Arguments = ArgumentStr.Split(',');
 
-            result.Add("Type", new List<string>());
-            result.Add("Argument", new List<string>());
+            result.Add(TYPE, new List<string>());
+            result.Add(VARIABLE, new List<string>());
+
+            for (int i=0; i<Arguments.Length; i++)
+            {
+                var Consist = Arguments[i].Trim().Split(' ');
+
+                if(Consist.Length > NORMAL_ARGUMENT)
+                {
+                    string Type = $"{Consist[0]} ";
+                    for(int j=1; j<Consist.Length - 1; j++)
+                    {
+                        Type += $"{Consist[j]} ";
+                    }
+                    Type = Type.Substring(0, Type.Length - 1);
+                    result[TYPE].Add($"const {Type}&");
+                    result[VARIABLE].Add(Consist[Consist.Length - 1]);
+                }
+                else
+                {
+                    result[TYPE].Add($"const {Consist[0]}&");
+                    result[VARIABLE].Add(Consist[1]);
+                }
+            }
 
             return result;
+        }
+
+        private string ReconsistLine(string Line)
+        {
+            StringBuilder ReLine = new("void ");
+
+            int Start = Line.IndexOf('(') + 1;
+            var result = AnalyzeLine(Line);
+
+            if (result.Count > 0)
+                ReLine.Insert(Start, "const MyList<Session*>& SessionList, ");
+            else
+                ReLine.Insert(Start, "const MyList<Session*>& SessionList");
+
+            // 각 매개변수 앞에 const Type& 이걸 붙여줘야 한다.
+
+
+            return ReLine.ToString();
         }
 
         public void MakeProxyHeaderFile()
@@ -63,13 +115,10 @@ namespace IDLCompiler.Service
 
             sb.AppendLine("#pragma once\n");
 
-            sb.AppendLine("class Proxy {");
-            sb.AppendLine("public:");
             for (int i=0; i<_ValidLines.Count; i++)
             {
-                sb.AppendLine(_ValidLines[i]);
+                sb.AppendLine($"void {_ValidLines[i]}");
             }
-            sb.AppendLine("};");
 
             File.WriteAllText($"{PROXY_H_FILE}", sb.ToString());
         }
@@ -79,22 +128,35 @@ namespace IDLCompiler.Service
             StringBuilder sb = new();
 
             sb.AppendLine($"#include \"{PROXY_H_FILE}\"");
-            sb.AppendLine($"#include \"Packet.h\"\n");
+            sb.AppendLine($"#include \"Packet.h\"");
             sb.AppendLine($"#include \"Session.h\"\n");
 
             for (int i = 0; i < _ValidLines.Count; i++)
             {
                 // 세미콜론 제거
-                sb.AppendLine(_ValidLines[i].Remove(';'));
+                sb.AppendLine($"void {_ValidLines[i].Substring(0, _ValidLines[i].Length - 1)}");
                 sb.AppendLine("{");
 
                 var Dict = AnalyzeLine(_ValidLines[i]);
+                if (Dict[VARIABLE].Count > EXCEPT_ARGUMENT)
+                {
+                    sb.Append("\tPacket packet;\n");
+                    sb.Append("\tpacket ");
+                    for (int j = EXCEPT_ARGUMENT; j < Dict[VARIABLE].Count; j++)
+                    {
+                        sb.Append($"<< {Dict[VARIABLE][j]} ");
+                    }
+                    sb[sb.Length - 1] = ';';
+                    sb.AppendLine();
 
-                sb.AppendLine("Packet ");
+                    sb.AppendLine($"\tSendMessage({Dict[VARIABLE][0]}, &packet);");
+                }
+                
+
                 sb.AppendLine("}\n");
             }
 
-            File.WriteAllText($"{PROXY_H_FILE}", sb.ToString());
+            File.WriteAllText($"{PROXY_CPP_FILE}", sb.ToString());
         }
 
         public void MakeStubHeaderFile()
