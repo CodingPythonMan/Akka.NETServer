e0f0919f9916bfa301e658b0511e9e415c1795b6
JuYoung Kim <robolab1902@naver.com>
2025-05-09 17:16:15 +0900

    차근차근... 아니 델리키는 왜 찾는거야.

---
+++ Core/HelloService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class HelloService
+    {
+        public string Greet(string name) => $"Hello, {name}!";
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,16 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core;
+
+class Program
+{
+    static void Main()
+    {
+        // 그럼 어케 만들징? ㅇㅅㅇ
+        var hello = new HelloService();
+        Console.WriteLine(hello.Greet("DeliKi"));
+    }
+}
